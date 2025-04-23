d803c1c8f7bf6fb49685da7b74061b1f0088ad29
Jatin_Mehta <mehtajatin045@gmail.com>
2025-04-24 00:41:19 +0530

    remove import

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,7 +1,7 @@
 export default function Navbar() {
     return (
         <nav className=" top-0 left-0 flex justify-between items-center right-0 h-[92px] w-[1623px] shadow-[0_4px_10px_rgba(182,182,182,0.1)] z-50 bg-white">
-            <div className="ml-[103px] font-bold font-ubranist">
+            <div className="ml-[103px] font-bold font-ubranist text-3xl ">
                 ← Stories
                 
             </div>
