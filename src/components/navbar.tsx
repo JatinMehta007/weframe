feeee9b3d1205554cc6d858ad0b70791c401d62d
Jatin_Mehta <mehtajatin045@gmail.com>
2025-04-24 02:46:34 +0530

    add responsiveness

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,18 +1,19 @@
+"use client"
 export default function Navbar() {
     return (
-        <nav className=" top-0 left-0 flex justify-between items-center right-0 h-[92px] w-[1623px] shadow-[0_4px_10px_rgba(182,182,182,0.1)] z-50 bg-white">
-            <div className="ml-[103px] font-bold font-ubranist text-3xl ">
+        <nav className=" top-0 left-0 flex justify-between w-[320px] items-center right-0  md:h-[92px] md:w-[1623px] shadow-[0_4px_10px_rgba(182,182,182,0.1)] z-50 bg-white">
+            <div className="md:ml-[103px] font-bold font-ubranist md:text-3xl  ">
                 ← Stories
                 
             </div>
-            <div className="mr-20 w-[297px] h-[53px] rounded-[8px] border-[1px] border-[#E8EFF7] justify-center items-center flex gap-[10px]">
-                <img src="/images/girl.png" alt="" className="w-[33.21px] h-[33.21px] rounded-[5px]"/>
-                <div className="w-[203px] h-[38px]  text-[#373B5C]">
-                    <p className="text-[10px] font-medium ">Welcome back,</p>
-                    <p className="text-[17px]">Akshita Patel</p>
+            <div className="md:mr-20 md:w-[297px] md:h-[53px] rounded-[8px] border-[1px] border-[#E8EFF7] justify-center items-center flex gap-[10px]">
+                <img src="/images/girl.png" alt="" className="md:w-[33.21px] md:h-[33.21px] w-5 h-5 rounded-[5px]"/>
+                <div className="md:w-[203px] md:h-[38px]  text-[#373B5C]">
+                    <p className="text-[8px] md:text-[10px] font-medium ">Welcome back,</p>
+                    <p className="md:text-[17px] text-[10px]">Akshita Patel</p>
                 </div>
                 <div>
-                    <img src="/images/arrow.png" alt="down-arrow" className="w-[15.5px] h-[8.5px] left-[4.25px] text-[#1E2875]" />
+                    <img src="/images/arrow.png" alt="down-arrow" className="w-[15.5px] h-[8.5px] md:left-[4.25px] text-[#1E2875] mr-2" />
                 </div>
             </div>
         </nav>
