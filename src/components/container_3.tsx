21367a27a8decbfb35eddc2b2bf47f951edf0e4a
Jatin_Mehta <mehtajatin045@gmail.com>
2025-04-24 00:04:12 +0530

    add images

--- src/components/container_3.tsx
+++ src/components/container_3.tsx
@@ -94,6 +94,14 @@ export default function Container_3() {
             />
           <div className="">
             <p className="w-[306px] h-[107px] left-[19px] absolute top-[308px]  text-white  font-semibold text-2xl">
+            <div>
+            <div className="bg-white absolute flex justify-center items-center bottom-[380px] left-[198px] rounded-[5px] w-[62px] h-[26px]  gap-1">
+            <img src="/images/eye1.png" alt="eye" className="h-[16px] w-[16px]"  /> <p className="text-[#1C1442] text-xs">428</p>
+            </div>
+            <div className="w-[36px] absolute h-[26px] left-[265px] rounded-[5px] gap-[5px] bottom-[380px] bg-white flex justify-center items-center">
+              <img src="/images/status-up copy.png" alt="" className="h-[16px] w-[16px] text-[#1C1442]" />
+            </div>
+            </div>
             Teknion wins Gold at 2022 International Design Awards
               <div className="flex  gap-[10px]">
             <p className="text-base mt-[17px] font-extrabold ">BUSINESS · <span className="text-[#A0A3BD] text-sm font-semibold">20 Sep 2022</span> </p>
@@ -128,6 +136,14 @@ export default function Container_3() {
             />
           <div className="">
             <p className="w-[310px] h-[107px] left-[19px] absolute top-[308px]  text-white  font-semibold text-2xl">
+            <div>
+            <div className="bg-white absolute flex justify-center items-center bottom-[380px] left-[198px] rounded-[5px] w-[62px] h-[26px]  gap-1">
+            <img src="/images/eye1.png" alt="eye" className="h-[16px] w-[16px]"  /> <p className="text-[#1C1442] text-xs">428</p>
+            </div>
+            <div className="w-[36px] absolute h-[26px] left-[265px] rounded-[5px] gap-[5px] bottom-[380px] bg-white flex justify-center items-center">
+              <img src="/images/status-up copy.png" alt="" className="h-[16px] w-[16px] text-[#1C1442]" />
+            </div>
+            </div>
             How 7 lines code turned into
             $36 Billion Empire
               <div className="flex  gap-[10px]">
@@ -165,6 +181,14 @@ export default function Container_3() {
             />
           <div className="">
             <p className="w-[302px] h-[107px] left-[19px] absolute top-[308px]  text-white  font-semibold text-2xl">
+            <div>
+            <div className="bg-white absolute flex justify-center items-center bottom-[380px] left-[198px] rounded-[5px] w-[62px] h-[26px]  gap-1">
+            <img src="/images/eye1.png" alt="eye" className="h-[16px] w-[16px]"  /> <p className="text-[#1C1442] text-xs">428</p>
+            </div>
+            <div className="w-[36px] absolute h-[26px] left-[265px] rounded-[5px] gap-[5px] bottom-[380px] bg-white flex justify-center items-center">
+              <img src="/images/status-up copy.png" alt="" className="h-[16px] w-[16px] text-[#1C1442]" />
+            </div>
+            </div>
               How 7 line code turned into $36 Billion Empire
               <div className="flex  gap-[10px]">
             <p className="text-base mt-[17px] font-extrabold ">BUSINESS · <span className="text-[#A0A3BD] text-sm font-semibold">20 Sep 2022</span> </p>
@@ -198,7 +222,17 @@ export default function Container_3() {
             className="w-[340px] h-[500px] object-cover rounded-xl  bg-no-repeat "
             />
           <div className="">
-            <p className="w-[302px] h-[107px] left-[19px] absolute top-[308px]  text-white  font-semibold text-2xl">Chez pierre restaurant in Monte Carlo by Vuidafieri
+            <p className="w-[302px] h-[107px] left-[19px] absolute top-[308px]  text-white  font-semibold text-2xl">
+            <div>
+            <div className="bg-white absolute flex justify-center items-center bottom-[380px] left-[198px] rounded-[5px] w-[62px] h-[26px]  gap-1">
+            <img src="/images/eye1.png" alt="eye" className="h-[16px] w-[16px]"  /> <p className="text-[#1C1442] text-xs">428</p>
+            </div>
+            <div className="w-[36px] absolute h-[26px] left-[265px] rounded-[5px] gap-[5px] bottom-[380px] bg-white flex justify-center items-center">
+              <img src="/images/status-up copy.png" alt="" className="h-[16px] w-[16px] text-[#1C1442]" />
+            </div>
+            </div>
+
+              Chez pierre restaurant in Monte Carlo by Vuidafieri
               <div className="flex  gap-[10px]">
             <p className="text-base mt-[17px] font-extrabold ">BUSINESS · <span className="text-[#A0A3BD] text-sm font-semibold">20 Sep 2022</span> </p>
 
@@ -233,6 +267,14 @@ export default function Container_3() {
             />
           <div className="">
             <p className="w-[306px] h-[107px] left-[19px] absolute top-[308px]  text-white  font-semibold text-2xl">
+            <div>
+            <div className="bg-white absolute flex justify-center items-center bottom-[380px] left-[198px] rounded-[5px] w-[62px] h-[26px]  gap-1">
+            <img src="/images/eye1.png" alt="eye" className="h-[16px] w-[16px]"  /> <p className="text-[#1C1442] text-xs">428</p>
+            </div>
+            <div className="w-[36px] absolute h-[26px] left-[265px] rounded-[5px] gap-[5px] bottom-[380px] bg-white flex justify-center items-center">
+              <img src="/images/status-up copy.png" alt="" className="h-[16px] w-[16px] text-[#1C1442]" />
+            </div>
+            </div>
             Teknion wins Gold at 2022 International Design Awards
               <div className="flex  gap-[10px]">
             <p className="text-base mt-[17px] font-extrabold ">BUSINESS · <span className="text-[#A0A3BD] text-sm font-semibold">20 Sep 2022</span> </p>
@@ -268,6 +310,14 @@ export default function Container_3() {
             />
           <div className="">
             <p className="w-[310px] h-[107px] left-[19px] absolute top-[308px]  text-white  font-semibold text-2xl">
+            <div>
+            <div className="bg-white absolute flex justify-center items-center bottom-[380px] left-[198px] rounded-[5px] w-[62px] h-[26px]  gap-1">
+            <img src="/images/eye1.png" alt="eye" className="h-[16px] w-[16px]"  /> <p className="text-[#1C1442] text-xs">428</p>
+            </div>
+            <div className="w-[36px] absolute h-[26px] left-[265px] rounded-[5px] gap-[5px] bottom-[380px] bg-white flex justify-center items-center">
+              <img src="/images/status-up copy.png" alt="" className="h-[16px] w-[16px] text-[#1C1442]" />
+            </div>
+            </div>
             How 7 lines code turned into
             $36 Billion Empire
               <div className="flex  gap-[10px]">
