135e17bc8fab0578f82ed9ac655ce3b492cf1a85
Jatin_Mehta <mehtajatin045@gmail.com>
2025-04-24 02:46:03 +0530

    add responsiveness

--- src/components/container_2.tsx
+++ src/components/container_2.tsx
@@ -1,25 +1,26 @@
+"use client"
 export default function Container_2() {
     return(
-<div className="w-[797px] h-[54px] gap-[15px] flex ml-[53px] mt-[20px]  " >
+<div className="md:w-[797px]  md:h-[54px]  gap-[15px] flex md:ml-[53px] mt-[20px]  " >
        
-        <div className="bg-[#1C1442] w-[119px] p-[15px] rounded-xl gap-[6px] text-white">
-        All (4,500)
+        <div className="bg-[#1C1442] md:w-[119px]  md:p-[15px] rounded-xl gap-[6px] text-white text-[6px] md:text-xl p-1 flex justify-center items-center ">
+        All <span>(4,500)</span>
         </div>
 
        
-        <div className=" w-[135px] p-[15px] rounded-xl gap-[6px] flex bg-[#E7E8EF] tracking-[-2%] font-semibold hover:bg-[#1C1442]  hover:text-white">
+        <div className=" md:w-[135px] md:p-[15px] rounded-xl gap-[6px]  bg-[#E7E8EF] tracking-[-2%] font-semibold hover:bg-[#1C1442]  hover:text-white text-[6px] md:text-xl p-1 flex justify-center items-center">
         Draft <span className="text-[#A0A3BD]">(1,203)</span>
         </div>
 
-        <div className=" w-[150px] hover:bg-[#1C1442]  hover:text-white p-[15px] rounded-xl gap-[6px] flex bg-[#E7E8EF] tracking-[-2%] font-semibold ">
+        <div className=" md:w-[150px] hover:bg-[#1C1442]  hover:text-white md:p-[15px] rounded-xl gap-[6px] flex bg-[#E7E8EF] tracking-[-2%] font-semibold text-[6px] md:text-xl p-1  justify-center items-center ">
         Pending <span className="text-[#A0A3BD]">(890)</span>
         </div>
 
-        <div className=" w-[176px] hover:bg-[#1C1442]  hover:text-white p-[15px] rounded-xl gap-[6px] flex bg-[#E7E8EF] tracking-[-2%] font-semibold ">
+        <div className=" md:w-[176px] hover:bg-[#1C1442]  hover:text-white md:p-[15px] rounded-xl gap-[6px] flex bg-[#E7E8EF] tracking-[-2%] font-semibold md:text-xl text-[6px] p-1  justify-center items-center">
         Published<span className="text-[#A0A3BD]">(2,432)</span>
         </div>
 
-        <div className=" w-[157px] hover:bg-[#1C1442] hover:text-white p-[15px] rounded-xl gap-[6px] flex bg-[#E7E8EF] tracking-[-2%] font-semibold ">
+        <div className="invisible md:visible md:w-[157px] hover:bg-[#1C1442] hover:text-white md:p-[15px] rounded-xl gap-[6px] flex bg-[#E7E8EF] md:text-xl tracking-[-2%] font-semibold text-[6px] p-1  justify-center items-center">
         Archived <span className="text-[#A0A3BD]">(320)</span>
         </div>
 
