42fca1ab61d7f75e2dbfaa6978362dc8ccc547c5
Jatin_Mehta <mehtajatin045@gmail.com>
2025-05-15 19:24:13 +0530

    add text

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,10 +1,9 @@
 "use client"
 export default function Navbar() {
     return (
-        <nav className=" top-0 left-0 flex justify-between w-[320px] items-center right-0  md:h-[92px] md:w-[1623px] shadow-[0_4px_10px_rgba(182,182,182,0.1)] z-50 bg-white">
-            <div className="md:ml-[103px] font-bold font-ubranist md:text-3xl  ">
+        <nav className="fixed  top-0 left-[300px] flex justify-between w-[320px] items-center right-0  md:h-[92px] md:w-[1623px] shadow-[0_4px_10px_rgba(182,182,182,0.1)] z-50 bg-white">
+            <div className="md:ml-[103px] font-bold font-ubranist md:text-3xl  lg:text-3xl ">
                 ← Stories
-                
             </div>
             <div className="md:mr-20 md:w-[297px] md:h-[53px] rounded-[8px] border-[1px] border-[#E8EFF7] justify-center items-center flex gap-[10px]">
                 <img src="/images/girl.png" alt="" className="md:w-[33.21px] md:h-[33.21px] w-5 h-5 rounded-[5px]"/>
